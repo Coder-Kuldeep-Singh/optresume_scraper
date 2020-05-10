6f77157b1546a553de18ded9b232ff8c65e911fa
mavensingh <kuldeep.avsar@gmail.com>
2020-05-10 13:09:00 +0530

    added function to extract jobs by title

---
+++ jobs/job.go
@@ -0,0 +1,32 @@
+package jobs
+
+import (
+	"log"
+
+	"github.com/optresume/client"
+	"github.com/optresume/reader"
+)
+
+type Jobs struct {
+	Title       string
+	Compnay     string
+	Location    string
+	Type        string
+	View        string
+	Description string
+	PostedTime  string
+}
+
+func JobsByLocation(URL string, jobTitle, pages string) (string, error) {
+	URL = URL + jobTitle + "-jobs?page_number=" + pages
+	resp, err := client.Request(URL)
+	if err != nil {
+		log.Printf("Proxy Failed {%v}\n", err)
+	}
+
+	output, err := reader.ReadAll(".careerfy-row", resp)
+	if err != nil {
+		return output, err
+	}
+	return output, err
+}
