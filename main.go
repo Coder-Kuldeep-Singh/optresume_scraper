16120f683ee21e9ff669bff9eb3043bceceae7dd
mavensingh <kuldeep.avsar@gmail.com>
2020-05-11 18:03:55 +0530

    calling function

--- main.go
+++ main.go
@@ -4,17 +4,19 @@ import (
 	"fmt"
 
 	"github.com/optresume/jobs"
+	"github.com/optresume/model"
 )
 
 func main() {
 	BaseUrl := "https://www.optresume.com/"
-	output, err := jobs.AllJobs(BaseUrl, 2)
+	output, err := jobs.AllJobs(BaseUrl, 100)
 	if err != nil {
 		fmt.Printf("Error to read response {%v}\n", err)
 		return
 	}
 	// for _, list := range output {
-	fmt.Println(output)
+	// fmt.Println(output)
+	model.Optresume(output)
 	// }
 	fmt.Println("************************************************************************************************************************************************")
 	fmt.Println("************************************************************************************************************************************************")
