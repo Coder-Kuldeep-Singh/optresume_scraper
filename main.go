156a769cb51dc2f0034574910d393bc9638d53a3
mavensingh <kuldeep.avsar@gmail.com>
2020-05-12 13:34:13 +0530

    modified

--- main.go
+++ main.go
@@ -2,32 +2,19 @@ package main
 
 import (
 	"fmt"
-	"sync"
 
 	"github.com/optresume/jobs"
 	"github.com/optresume/model"
 )
 
 func main() {
-	var wg sync.WaitGroup
 	BaseUrl := "https://www.optresume.com/"
-	wg.Add(1)
-	go func() {
-
-		output, err := jobs.AllJobs(BaseUrl, 100, &wg)
-		if err != nil {
-			fmt.Printf("Error to read response {%v}\n", err)
-			return
-		}
-		// for _, list := range output {
-		// fmt.Println(output)
-		wg.Add(1)
-		go func() {
-			model.Optresume(output, &wg)
-		}()
-	}()
-	// }
-	wg.Wait()
+	output, err := jobs.AllJobs(BaseUrl, 1)
+	if err != nil {
+		fmt.Printf("Error to read response {%v}\n", err)
+		return
+	}
+	model.Optresume(output)
 	fmt.Println("************************************************************************************************************************************************")
 	fmt.Println("************************************************************************************************************************************************")
 	fmt.Println("Execution Finished")
